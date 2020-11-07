0ec058734e9e50a54e6cb4c04d59280501b7ea07
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2020-11-07 14:23:37 -0500

    Canvas dimensions based on remaining space

--- src/simulator/PoolSideView.js
+++ src/simulator/PoolSideView.js
@@ -1,6 +1,16 @@
+let canvasWidth = 500;
+let canvasHeight = 500;
+
+export const setCanvasDimensions = (width, height) => {
+    canvasWidth = width;
+    canvasHeight = height;
+}
+
+
 export const p5script = (p5) => {
     p5.setup = () => {
-        p5.createCanvas(500, 500)
+        const canvas = p5.createCanvas(canvasWidth, canvasHeight);
+        canvas.parent("canvas-wrapper")
         p5.background("grey")
     }
     p5.draw = () => {

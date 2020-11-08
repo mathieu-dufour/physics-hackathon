f75edf17b1c7fd890d4afbfa06294254af791624
laurentsiino <laurentsiino@gmail.com>
2020-11-07 23:35:03 -0500

    Good default values, better cornstarch viscosity

--- src/models/FluidList.js
+++ src/models/FluidList.js
@@ -11,7 +11,7 @@ export default [
         "name": "Corn starch",
         "viscosityFunction": function (velocity) {
             // TODO: Change corn starch viscosity function
-            if (velocity > 1){
+            if (velocity > 5){
                 console.log("corn")
                 return 200000000000 * velocity
             } else {return 0.00000000002 * velocity}
